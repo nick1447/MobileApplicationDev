4c824f042e256ded1bb71533fec54affe157cb9f
Nicholas L'Anglais <nick.lawnboy@gmail.com>
2022-09-21 12:58:52 -0400

    Adding compound buttons

---
+++ app/src/main/java/algonquin/cst2335/id040929818/ui/MainActivity.java
@@ -0,0 +1,61 @@
+package algonquin.cst2335.id040929818.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import algonquin.cst2335.id040929818.data.MainViewModel;
+import algonquin.cst2335.id040929818.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding variableBinding;
+    private MainViewModel model;
+
+    @Override
+    //test
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+
+        model = new ViewModelProvider(this).get(MainViewModel.class);
+
+
+        variableBinding.button.setOnClickListener(click -> {
+            model.editString.postValue(variableBinding.edittext.getText().toString());
+        });
+
+        model.editString.observe(this, string -> {
+            variableBinding.textview.setText("Your edit text has " + string);
+        });
+
+        model.isChecked.observe(this, isChecked -> {
+            variableBinding.radiobutton.setChecked(isChecked);
+            variableBinding.switchbutton.setChecked(isChecked);
+            variableBinding.checkbox.setChecked(isChecked);
+
+            Toast.makeText(this, "The value is now: " + isChecked, Toast.LENGTH_SHORT).show();
+        });
+
+        variableBinding.checkbox.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.isChecked.postValue(isChecked);
+        });
+
+        variableBinding.switchbutton.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.isChecked.postValue(isChecked);
+        });
+
+        variableBinding.radiobutton.setOnCheckedChangeListener((btn, isChecked) -> {
+            model.isChecked.postValue(isChecked);
+        });
+
+    }
+}
