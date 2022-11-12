b7561b89496aa0096a14dcb301585efdc756a92c
Nicholas L'Anglais <nick.lawnboy@gmail.com>
2022-11-12 13:53:27 -0400

    Add mainActivity

--- app/src/main/java/algonquin/cst2335/id040929818/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/id040929818/MainActivity.java
@@ -2,13 +2,69 @@ package algonquin.cst2335.id040929818;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+
+import algonquin.cst2335.id040929818.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SharedPreferences prefs;
+
     @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("MainActivity", "onDestroy(): Any memory used by the application is freed..");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "onResume(): The application is now responding to user input.");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("MainActivity", "onStop(): The application is no longer visible.");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w("MainActivity", "onStart(): The application is now visible on screen.");
+    }
+
+    @Override
+    //test
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        ActivityMainBinding variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+
+        prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+
+        variableBinding.button.setOnClickListener(click -> {
+            startActivity(
+                    new Intent(MainActivity.this, SecondActivity.class)
+                            .putExtra("EmailAddress", variableBinding.emailAddressEdittext.getText().toString())
+            );
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putString("LoginName", variableBinding.emailAddressEdittext.getText().toString());
+            editor.apply();
+        });
+
+
+
+
+        variableBinding.emailAddressEdittext.setText(prefs.getString("LoginName", ""));
+
+        //Log.w( "MainActivity", "In onCreate() - Loading Widgets" );
     }
 }
