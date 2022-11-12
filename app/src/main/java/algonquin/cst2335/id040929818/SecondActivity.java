02e9f5170bc54a93404e9cee21ab466746aa98be
Nicholas L'Anglais <nick.lawnboy@gmail.com>
2022-11-12 13:56:20 -0400

    added second acticity

---
+++ app/src/main/java/algonquin/cst2335/id040929818/SecondActivity.java
@@ -0,0 +1,109 @@
+package algonquin.cst2335.id040929818;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import algonquin.cst2335.id040929818.databinding.ActivitySecondBinding;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private ActivitySecondBinding variableBinding;
+    private SharedPreferences prefs;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        variableBinding = ActivitySecondBinding.inflate(getLayoutInflater());
+        setContentView(variableBinding.getRoot());
+
+        prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+
+        Intent previousIntent = getIntent();
+        String email = previousIntent.getStringExtra("EmailAddress");
+
+        TextView textView = variableBinding.textView;
+        textView.setText(String.format("Welcome back %s", email));
+
+        variableBinding.callNumberButton.setOnClickListener(click -> {
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            call.setData(Uri.parse("tel:" + variableBinding.editTextPhone.getText().toString()));
+            startActivity(call);
+        });
+
+        ActivityResultLauncher<Intent> cameraResultIntent = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+                    if(result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        Bitmap image = data.getParcelableExtra("data");
+                        variableBinding.imageView.setImageBitmap(image);
+                        FileOutputStream fileOut;
+                        try {
+                            fileOut = openFileOutput("Test.png", Context.MODE_PRIVATE);
+                            image.compress(Bitmap.CompressFormat.PNG, 100, fileOut);
+                            Log.w("SecondActivity", getFilesDir().toString());
+                            fileOut.flush();
+                            fileOut.close();
+                            File inputFile = new File(getFilesDir(), "Test.png");
+                            if(inputFile.exists()) {
+                                Log.w("SecondActivity", Boolean.toString(inputFile.exists()));
+                            }
+                        } catch(IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+        );
+
+        variableBinding.changeImageButton.setOnClickListener(click -> {
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            cameraResultIntent.launch(cameraIntent);
+        });
+
+        //If a picture has already been set then load it into the ImageView
+        File inputFile = new File(getFilesDir(), "Test.png");
+        if(inputFile.exists()) {
+            Log.w("SecondActivity", "file exists");
+            Bitmap image = BitmapFactory.decodeFile(getFilesDir().getPath() + "/Test.png");
+            variableBinding.imageView.setImageBitmap(image);
+        } else {
+            Log.w("SecondActivity", "file does not exist");
+        }
+
+        if(prefs.getString("PhoneNumber", "") != "") {
+            variableBinding.editTextPhone.setText(prefs.getString("PhoneNumber", ""));
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences prefs = getSharedPreferences("MyData", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString("PhoneNumber", variableBinding.editTextPhone.getText().toString());
+        editor.apply();
+    }
+}
